765122a5746cb9cd8a77d6d09f6ebe0f301ff6cc
MIKI Takushi <takushi-m@users.noreply.github.com>
2019-08-08 21:56:48 +0900

    コメントを記録し、特定コマンドで出力する

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+log = []
+
+def repl():
+    line = input()
+    if line == "view":
+        for l in log:
+            print(l)
+    else:
+        print(">",line)
+        log.append(line)
+
+def main():
+    while True:
+        repl()
+
+if __name__ == '__main__':
+    main()
